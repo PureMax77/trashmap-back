ea2e1172daa225e61bce455995d3fe6cd8690d4f
PureMax <ertr777@naver.com>
2021-08-12 19:58:25 +0900

    원복

--- src/t_mountain/seeTMountains/seeTMountains.resolvers.js
+++ src/t_mountain/seeTMountains/seeTMountains.resolvers.js
@@ -3,13 +3,6 @@ import { protectedResolver } from "../../users/users.utils"
 
 export default {
   Query: {
-    seeTMountains: () => {
-      try {
-        return client.t_Mountain.findMany()
-      } catch (e) {
-        console.log(e)
-        return []
-      }
-    },
+    seeTMountains: () => client.t_Mountain.findMany(),
   },
 }
