2dc1d353b03cd505c48214f70bd282ca2451e508
PureMax <ertr777@naver.com>
2021-08-19 15:52:11 +0900

    수정

--- src/test/test.resolvers.js
+++ src/test/test.resolvers.js
@@ -1,9 +1,15 @@
+import client from "../client"
 import { deleteInS3 } from "../shared/shared.utils"
 
 export default {
   Mutation: {
     test: async (_, { url }) => {
-      await deleteInS3(url)
+      await client.t_Mountain.updateMany({
+        data: {
+          image:
+            "https://trashmap-fold.s3.ap-northeast-2.amazonaws.com/TMountain-img/defaultMountain.jpg",
+        },
+      })
       return {
         ok: true,
       }
